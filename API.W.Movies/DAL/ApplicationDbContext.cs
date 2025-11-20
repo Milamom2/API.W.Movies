a38f83c503b6e07207c0d0e4429c0e6d439ced00
MARIA CAMILA MONROY BILBAO <mariamonroy1122932@correo.itm.edu.co>
2025-11-20 07:37:47 -0500

    Add project files.

---
+++ API.W.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.W.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.W.Movies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Seccion para crear el DbSet de las entidades o modelos
+        public DbSet<Category> Categories { get; set; }
+    }
+}
