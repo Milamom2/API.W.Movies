feb9c3e1fdcbb2471bba8842c85b3ff0bbe94ca8
MARIA CAMILA MONROY BILBAO <mariamonroy1122932@correo.itm.edu.co>
2025-11-25 14:58:08 -0500

    New CategoriesController and CategoryService created

--- API.W.Movies/Repository/CategoryRepository.cs
+++ API.W.Movies/Repository/CategoryRepository.cs
@@ -6,7 +6,7 @@ namespace API.W.Movies.Repository
 {
     public class CategoryRepository : ICategoryRepository
     {
-       private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
         public CategoryRepository(ApplicationDbContext context)
         {
             _context = context;
@@ -43,7 +43,7 @@ namespace API.W.Movies.Repository
             }
             _context.Categories.Remove(category); //3) si existe la elimino
             return await SaveAsync();
-            }
+        }
 
         public async Task<ICollection<Category>> GetCategoriesAsync()
         {
@@ -66,13 +66,14 @@ namespace API.W.Movies.Repository
             category.ModifiedDate = DateTime.UtcNow;
             _context.Categories.Update(category);
             return await SaveAsync();
-        {
-            
+        }
+
         private async Task<bool> SaveAsync()
         {
             return await _context.SaveChangesAsync() >= 0 ? true : false;
         }
     }
+}
 
 
        
---
+++ API.W.Movies/Services/CategoryService.cs
@@ -0,0 +1,56 @@
+﻿using API.W.Movies.DAL.Models;
+using API.W.Movies.DAL.Models.Dtos;
+using API.W.Movies.Repository;
+using API.W.Movies.Services.IServices;
+using AutoMapper;
+
+namespace API.W.Movies.Services
+{
+    
+    public class CategoryService: ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDto> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
+        {
+            var categories = _categoryRepository.GetCategoriesAsync(); //Llamando el metodo desde la capa de repositorio
+            return _mapper.Map<ICollection<CategoryDto>>(categories); //Mapeando la entidad a DTO
+            
+        }
+
+        public Task<bool> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
